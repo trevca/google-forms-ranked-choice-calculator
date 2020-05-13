b55e3442f02f981e7e110d704971453c1b4abe83
Ryan Buchner <Ryan@Ryans-MacBook-Pro-3.local>
2020-05-13 14:30:13 -0700

    Takes in dick

--- calculate_winners.py
+++ calculate_winners.py
@@ -7,41 +7,60 @@ def in_list(item, lisp):
             return True
     return False
 
-def find_first_winner(votes,position,still_in_it,total_number_of_candidates_for_everything):   
+def rev_search_dict(dictionary,value):
+    key_list=list(dictionary.keys())
+    val_list=list(dictionary.values())
+    return key_list[val_list.index(value)]
+
+def find_first_winner(votes,position,still_in_it):   
     number_moving_on=2
     length=len(votes[0][position])
-    ranking=np.zeros(total_number_of_candidates_for_everything).tolist()
+    candidate_key={}
+    i=0
+    for candidate in still_in_it[position]:
+        candidate_key[i]=candidate
+        i+=1
+    ranking=np.zeros(len(candidate_key)).tolist()
     for vote in votes:
         for i in range(0,length-1):
-            if (in_list(vote[position][i],still_in_it)):
-                choice=vote[position][i]
-                ranking[choice]+=1
+            if in_list(vote[position][i],still_in_it[position]):
+                choice_name=vote[position][i]
+                choice_index=rev_search_dict(candidate_key,choice_name)
+                ranking[choice_index]+=1
                 break
     scores=ranking.copy()
     scores.sort()
     scores.reverse()
     most=scores[0]
     if most>(len(votes)/2):
-        return ranking.index(most)
+        return candidate_key.get(ranking.index(most))
+    elif len(still_in_it[position])==2:
+        return "Tie"
     else:
         second_most=scores[1]
-        while true:
-            next=scores[number_moving_on-1]
-            if(next==second_most_scores):
-                number_moving_on+=1
+        nexxt=scores[number_moving_on]
+        while nexxt==second_most:
+            number_moving_on+=1
+            if (len(scores)-1)>=number_moving_on:
+                nexxt=scores[number_moving_on]
             else:
                 break
-        still_in_it=[]
+        new_still_in_it={position:[]}
         first=ranking.index(most)
-        still_in_it.append(first)
         ranking[first]=0
-        for i in range(0,number_moving_on-2):
+        first=candidate_key[first]
+        new_still_in_it[position].append(first)
+        for i in range(0,number_moving_on-1):
             second=ranking.index(second_most)
-            still_in_it.append(second)
             ranking[second]=0
-        find_first_winner(votes,position,still_in_it,total_number_of_candidates_for_everything)
+            second=candidate_key[second]
+            new_still_in_it[position].append(second)
+        if len(still_in_it[position])==len(new_still_in_it[position]):
+            return "Tied"
+        return find_first_winner(votes,position,new_still_in_it)
 
 
-yippee=[{"President":[1,0],"VP":[0,2,1]},{"President":[0,1],"VP":[0,1,2]},{"President":[0,1],"VP":[0,1,2]}]
-print(find_first_winner(yippee,"President",[0,1]).3)
+votes=[{"President":["Trevor","Ryan","Kenton"]},{"President":["Ryan","Trevor","Kenton"]},{"President":["Kenton","Ryan","Trevor"]},{"President":["Trevor","Ryan","Kenton"]},{"President":["Ryan","Trevor","Kenton"]}]
+candidates={"President":["Trevor","Ryan","Kenton"]}
+print(find_first_winner(votes,"President",candidates))
 
