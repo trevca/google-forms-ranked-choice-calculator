f5061ea1c9e07692b8ca2842cc5b49f3c8c46c56
trevca <trevorcarpenter4@gmail.com>
2020-05-12 21:29:42 -0700

    done with my part

---
+++ calculate_winners_Trevor.py
@@ -0,0 +1,5 @@
+from form_to_structure import voteInfo
+
+voting = voteInfo("candidate_preference.csv", "test_votes.csv")
+
+print(voting.getVotes())
--- form_to_structure.py
+++ form_to_structure.py
@@ -1,2 +1,31 @@
-pres_length = 1
-vp_length = 1
+import numpy as np
+import pandas as pd
+
+class voteInfo:
+    def __init__(self, candidate_info_path, vote_info_path):
+        self.candidates = pd.read_csv(candidate_info_path)
+        self.lengths = {"President": 1, "Vice President": 1, "Corporate Liaison": 6, "Secretary": 9, "Treasurer": 3, "UC Liaison": 2, 
+          "Director of Community Service": 3, "Director of Outreach": 2, "Director of Public Relations": 5, "Webmaster": 3}
+        self.votes = []
+
+        votes_data = pd.read_csv(vote_info_path).drop("Timestamp",1).drop("Email Address",1).to_numpy()
+        for vote in votes_data:
+            dictionary = {}
+            currPartition = 0
+            nextPartition = 0
+            for position in self.lengths:
+                currPartition = nextPartition
+                nextPartition += self.lengths[position]
+                dictionary[position] = vote[currPartition:nextPartition].tolist()
+            self.votes.append(dictionary)
+
+    def getVotes(self):
+        return self.votes
+
+    def getCandidates(self):
+        return self.candidates
+
+    def getLengths(self):
+        return self.lengths
+
+    
