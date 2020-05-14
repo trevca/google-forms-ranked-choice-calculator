6ccc69b65aa1f1d630728a85ba14534647f936b0
trevca <trevorcarpenter4@gmail.com>
2020-05-14 12:30:47 -0700

    small update

--- calculate_winners_Trevor.py
+++ calculate_winners_Trevor.py
@@ -3,3 +3,5 @@ from form_to_structure import voteInfo
 voting = voteInfo("candidate_preference.csv", "test_votes.csv")
 
 print(voting.getVotes())
+print(voting.getCandidates())
+print(voting.getLengths())
