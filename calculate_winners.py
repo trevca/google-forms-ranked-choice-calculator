ff85ae45906f26feb11295acd35f11e9e6e1cb2c
Ryan Buchner <Ryan@Ryans-MacBook-Pro-3.local>
2020-05-14 12:01:19 -0700

    finsihed needs testing

--- calculate_winners.py
+++ calculate_winners.py
@@ -57,7 +57,7 @@ def winner(votes,position,still_in_it):
             second=candidate_key[second]
             new_still_in_it[position].append(second)
         if len(still_in_it[position])==len(new_still_in_it[position]):
-            return "Tied"
+            return "Tie"
         return winner(votes,position,new_still_in_it)
 
 def get_first_winners (votes,candidates):
@@ -67,18 +67,7 @@ def get_first_winners (votes,candidates):
         first_winners[position]=win
     return first_winners    
 
-def real_winner(winners,preferences,real_winners,candidates):
-    new_real_winners=real_winners.copy()
-    for position in list(winners.keys()):
-        if preferences[winners[position]]==position:
-            new_real_winners[position]=winners[position]
-    if len(real_winners)==len(new_real_winners)
-        return new_real_winners #kinda running on assumption that at least someone won their first position, otherwise gets stuck in infinite loop
-    else:
-        removal(new_real_winners,preferences,candidates)
-        return real_winner(winners,preferences,new_real_winners,candidates)
-
-def remove_won_positions_and_won_people_from_preferences (real_winners,preferences):
+def remove_from_preferences (real_winners,preferences):
     positions_won=list(real_winners.keys())
     candidates_won=list(real_winners.values())
     candidates=list(preferences.keys())
@@ -89,22 +78,45 @@ def remove_won_positions_and_won_people_from_preferences (real_winners,preferenc
     for candidate in candidates_won
         preferences[candidate]=[]
 
-def remove_real_winners_from_candidates (reaL_winners,candidates):
+def remove_from_candidates (reaL_winners,candidates):
     candidates_won=list(real_winners.values())
+    positions_won=list(real_winners.keys())
     positions=list(candidates.keys())
     for candidate in candidates_won:
         for position in positions:
             if in_list(candidate,candidates[position]):
                 candidates[position].remove(candidate)
+    for position in positions_won:
+        candidate.pop(position)
 
 def removal(real_winners,preferences,candidates):
-    remove_won_positions_and_won_people_preferences (real_winners,preferences)
-    remove_real_winners_from_candidates (reaL_winners,candidates)
+    remove_from_preferences (real_winners,preferences)
+    remove_from_candidates (reaL_winners,candidates)
 
+def real_winner(winners,preferences,real_winners,candidates):
+    new_real_winners={}
+    for position in list(winners.keys()):
+        if preferences[winners[position]]==position:
+            new_real_winners[position]=winners[position]
+    real_winners.update(new_real_winners)
+    if len(new_real_winners)==0
+        return real_winners #kinda running on assumption that at least someone won their first position, otherwise gets stuck in infinite loop
+    else:
+        removal(new_real_winners,preferences,candidates)
+        return real_winner(winners,preferences,new_real_winners,candidates)
+
+def find_winners(preferences,candidates,real_winners,preferences):
+    if len(real_winners)==position_number:
+        return real_winners
+    else:
+        winners=get_first_winners(votes,candidates)
+        real_winner(winners,preferenes,reaL_winners,candidates)
+        return find_winners(preferences,preferences,real_winners,candidates)
 
 votes=[{"President":["Trevor","Ryan","Kenton"],"Vice President":["Ryan","Trevor","Kenton"]},{"President":["Ryan","Trevor","Kenton"],"Vice President":["Ryan","Trevor","Kenton"]},{"President":["Kenton","Ryan","Trevor"],"Vice President":["Kenton","Trevor","Ryan"]},{"President":["Trevor","Ryan","Kenton"],"Vice President":["Trevor","Ryan","Kenton"]},{"President":["Ryan","Trevor","Kenton"],"Vice President":["Kenton","Trevor","Ryan"]}]
 candidates={"President":["Trevor","Ryan","Kenton"],"Vice President":["Ryan","Trevor","Kenton"]}
 preferences=["Trevor":["President","Vice President"],"Kenton":["President","Vice President"],"Ryan":["Vice President","President"]}
+position_number=len(candidates)
 first_winners=get_first_winners(votes,candidates)
 real_winners={}
 real_winners=real_winner(first_winners,preferences,real_winners,candidates)
@@ -112,6 +124,5 @@ real_winners=real_winner(first_winners,preferences,real_winners,candidates)
 re run elections
 get_all winners who won first choice
 real_winners
-
-
+run until: len(real_winners)==position_number
 
