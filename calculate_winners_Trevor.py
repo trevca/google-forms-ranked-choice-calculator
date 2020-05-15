ca70b79827300d1cdaaa2bce2fe16d1c43d61172
trevca <trevorcarpenter4@gmail.com>
2020-05-15 16:20:48 -0700

    some changes

--- calculate_winners_Trevor.py
+++ calculate_winners_Trevor.py
@@ -5,3 +5,22 @@ voting = voteInfo("candidate_preference.csv", "test_votes.csv")
 print(voting.getVotes())
 print(voting.getCandidates())
 print(voting.getLengths())
+
+def get_first_winners (votes,candidates):
+    first_winners={}
+    for position in list(candidates.keys()):
+        win=winner(votes,position,candidates)
+        first_winners[position]=win
+    return first_winners    
+
+def remove_from_preferences (real_winners,preferences):
+    positions_won=list(real_winners.keys())
+    candidates_won=list(real_winners.values())
+    candidates=list(preferences.keys())
+    for position in positions_won:
+        for candidate in candidates:
+            if in_list(position,preferences[candidate]):
+                preferences[candidate].remove(position)
+    for candidate in candidates_won:
+        preferences[candidate]=[]
+
--- form_to_structure.py
+++ form_to_structure.py
@@ -3,7 +3,14 @@ import pandas as pd
 
 class voteInfo:
     def __init__(self, candidate_info_path, vote_info_path):
-        self.candidates = pd.read_csv(candidate_info_path)
+        candidate_raw = pd.read_csv(candidate_info_path).to_numpy()
+        self.candidates = {}
+        for candidate in candidate_raw:
+            self.candidates[candidate[0]] = []
+            self.candidates[candidate[0]].append(candidate[1])
+            self.candidates[candidate[0]].append(candidate[2])
+            self.candidates[candidate[0]].append(candidate[3])
+
         self.lengths = {"President": 1, "Vice President": 1, "Corporate Liaison": 6, "Secretary": 9, "Treasurer": 3, "UC Liaison": 2, 
           "Director of Community Service": 3, "Director of Outreach": 2, "Director of Public Relations": 5, "Webmaster": 3}
         self.votes = []
